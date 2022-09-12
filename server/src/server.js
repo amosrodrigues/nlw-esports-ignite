6123a5d9bf69577f064371a609bc5b7332a25924
Amós Rodrigues <amos.adm.rh@gmail.com>
2022-09-11 22:38:59 -0300

    feat: init setup server.

---
+++ server/src/server.js
@@ -0,0 +1,12 @@
+// const express = require('express');   "type": "commonjs",
+import express from 'express';
+const app = express();
+const PORT = 3333;
+app.get('/ads', (req, res) => {
+    return res.status(200).json([
+        { id: 1, name: 'Anúncio 1' },
+        { id: 2, name: 'Anúncio 2' },
+        { id: 3, name: 'Anúncio 3' },
+    ]);
+});
+app.listen(PORT, console.log(`Rodando na porta - ${PORT}`));
