2b85346ccb06da65aa798379147d7b050aec5030
Amós Rodrigues <amos.adm.rh@gmail.com>
2022-09-13 11:27:10 -0300

    chore: add assets in project

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,27 +1,10 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
-
-interface ButtonProps {
-  title: string;
-}
-
-function Button({ title }: ButtonProps) {
-  return (
-    <TouchableOpacity>
-      <Text>{title}</Text>
-    </TouchableOpacity>
-  );
-}
+import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hello React Native!</Text>
-
-      <Button title="Send 1" />
-      <Button title="Send 2" />
-      <Button title="Send 3" />
-
+      <Text>React Native!</Text>
       <StatusBar style="auto" />
     </View>
   );
