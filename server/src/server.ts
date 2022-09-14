f767af270cdddfef37db16c3ed78130b445dbb36
Amós Rodrigues <amos.adm.rh@gmail.com>
2022-09-14 09:47:42 -0300

    feat: implement routes of the api.

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,36 +1,101 @@
 // const express = require('express');   "type": "commonjs",
+// validar com zod
 import express from 'express';
+import cors from 'cors';
+
 import { PrismaClient } from '@prisma/client';
+import { convertHourStringToMinutes } from './utils/convert-hour-string-to-minutes';
+import { convertMinutesToHourString } from './utils/conert-minutes-to-hour-string';
 
 const app = express();
+
+app.use(express.json());
+app.use(cors());
+
 const prisma = new PrismaClient({ log: ['query'] });
 
 const PORT = 3333;
 
 app.get('/games', async (req, res) => {
-  const games = await prisma.game.findMany();
+  const games = await prisma.game.findMany({
+    include: {
+      _count: {
+        select: {
+          ads: true,
+        },
+      },
+    },
+  });
 
   return res.status(200).json(games);
 });
 
-app.post('/ads', (req, res) => {
-  return res.status(201).json([]);
+app.post('/games/:id/ads', async (req, res) => {
+  const gameId = req.params.id;
+  const body = req.body;
+
+  const ad = await prisma.ad.create({
+    data: {
+      gameId,
+      name: body.name,
+      yearsPlaying: body.yearsPlaying,
+      discord: body.discord,
+      weekDays: body.weekDays.join(','),
+      hourStart: convertHourStringToMinutes(body.hourStart),
+      hourEnd: convertHourStringToMinutes(body.hourEnd),
+      useVoiceChannel: body.useVoiceChannel,
+    },
+  });
+
+  return res.status(201).json(ad);
 });
 
-app.get('/games/:id/ads', (req, res) => {
-  // const gameId = req.params.id;
+app.get('/games/:id/ads', async (req, res) => {
+  const gameId = req.params.id;
 
-  return res.status(200).json([
-    { id: 1, name: 'Anúncio 1' },
-    { id: 2, name: 'Anúncio 2' },
-    { id: 3, name: 'Anúncio 3' },
-  ]);
+  const ads = await prisma.ad.findMany({
+    select: {
+      id: true,
+      name: true,
+      weekDays: true,
+      useVoiceChannel: true,
+      yearsPlaying: true,
+      hourEnd: true,
+      hourStart: true,
+    },
+    where: {
+      gameId,
+    },
+    orderBy: {
+      createdAt: 'desc',
+    },
+  });
+
+  return res.status(200).json(
+    ads.map((ad) => {
+      return {
+        ...ad,
+        weekDays: ad.weekDays.split(','),
+        hourStart: convertMinutesToHourString(ad.hourStart),
+        hourEnd: convertMinutesToHourString(ad.hourEnd),
+      };
+    }),
+  );
 });
 
-app.get('/ads/:id/discord', (req, res) => {
-  // const adId = req.params.id;
+app.get('/ads/:id/discord', async (req, res) => {
+  const adId = req.params.id;
 
-  return res.status(200).json([]);
+  const ad = await prisma.ad.findUniqueOrThrow({
+    select: {
+      discord: true,
+    },
+    where: {
+      id: adId,
+    },
+  });
+
+  return res.status(200).json({ discord: ad.discord });
 });
 
 app.listen(PORT, console.log(`Rodando na porta - ${PORT}`)!);
